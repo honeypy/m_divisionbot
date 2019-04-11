09e4eca9fa24be9d5ee44d7e6448836adc48d00c
honeypy <vitaly.esipov@gmail.com>
2019-04-11 16:02:10 +0300

    pusg tickets bets

--- bot.py
+++ bot.py
@@ -81,11 +81,11 @@ def send(bot, update):
     elif update.message.chat.id == 47303188 and update.message.text == 'test':
         user_ids = [47303188, ]
         buttons_list = [[InlineKeyboardButton('МЕНЮ', callback_data='back_main'), InlineKeyboardButton('УСПЕТЬ КУПИТЬ', url='https://radario.ru/widgets/mobile/385838')]]
-        keyboard = [[InlineKeyboardButton('<< в начало', callback_data='back_main'), InlineKeyboardButton('УСПЕТЬ КУПИТЬ', url='https://radario.ru/widgets/mobile/385838')]]
-        menu = build_menu(keyboard, 1)
-        markup = InlineKeyboardMarkup(menu)
-        print(markup)
-        print(user_ids)
+        # keyboard = [[InlineKeyboardButton('<< в начало', callback_data='back_main'), InlineKeyboardButton('УСПЕТЬ КУПИТЬ', url='https://radario.ru/widgets/mobile/385838')]]
+        # menu = build_menu(keyboard, 1)
+        # markup = InlineKeyboardMarkup(menu)
+        # print(markup)
+        # print(user_ids)
         count = 0
         for user in user_ids:
             # buttons_list = [[InlineKeyboardButton('<< в начало', callback_data='back_main'), InlineKeyboardButton('УСПЕТЬ КУПИТЬ', url='https://radario.ru/widgets/mobile/385838')]]
