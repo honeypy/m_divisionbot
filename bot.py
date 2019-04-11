09a7a7efb99f540eae15689e7e85ab8fcdd2deac
honeypy <vitaly.esipov@gmail.com>
2019-04-11 15:42:13 +0300

    pusg tickets bets

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ def send(bot, update):
                 pass
         print(count)
     elif update.message.chat.id == 47303188 and update.message.text == 'test':
-        user_ids = [47303188]
+        user_ids = [47303188, ]
         buttons_list = [[InlineKeyboardButton('МЕНЮ', callback_data='back_main'), InlineKeyboardButton('УСПЕТЬ КУПИТЬ', url='https://radario.ru/widgets/mobile/385838')]]
         keyboard = [[InlineKeyboardButton('<< в начало', callback_data='back_main'), InlineKeyboardButton('УСПЕТЬ КУПИТЬ', url='https://radario.ru/widgets/mobile/385838')]]
         menu = build_menu(keyboard, 1)
